69a74e54d16583781ae4dc8c42ec51c2fa5cbf22
giddob <gideonoblokuteye@gmail.com>
2022-10-09 18:46:06 +0000

    change of database

--- alifewithhim/settings.py
+++ alifewithhim/settings.py
@@ -77,8 +77,11 @@ WSGI_APPLICATION = 'alifewithhim.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'udwcrkhy',
+        'USER': 'udwcrkhy',
+        'PASSWORD': 'Tu6AF6pRyjSUyJDZMlEoxVXKoFvBsRJf',
+        'HOST': 'peanut.db.elephantsql.com',
     }
 }
 
