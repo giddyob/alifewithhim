12609490fdf714193190392ab68d297a5299b846
giddob <gideonoblokuteye@gmail.com>
2022-11-20 12:31:08 +0000

    static file error fixed

--- alifewithhim/settings.py
+++ alifewithhim/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-=nrr7rvo7q(m*+1s+_m^tb=w+r39*^8e0&f$k+7w(+4f8ts22m'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -120,7 +120,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
