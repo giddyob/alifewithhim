45b0954234faeaf9629ddd82ead1777d052e31f3
giddob <gideonoblokuteye@gmail.com>
2022-12-01 08:07:04 +0000

    new updates

--- registerapp/views.py
+++ registerapp/views.py
@@ -124,12 +124,6 @@ def verify(request, rg_id):
     messages.success(request, "succesfully verified")
     return redirect('verification')
 
-def unverify(request, rg_id):
-    registration = Registration.objects.get(id=rg_id)
-    registration.verified = False
-    registration.save()
-    return redirect('verified')
-
 def admin_verify(request, reg_id):
     registration = Registration.objects.get(id=reg_id)
     registration.verified = True
@@ -162,6 +156,12 @@ def unverified_total():
         if reg.verified == False:
             total_unverified +=1
     return total_unverified
+
+def unverify(request, reg_id):
+    registration = Registration.objects.get(id=reg_id)
+    registration.verified = False
+    registration.save()
+    return redirect('dashboard')
 
 
 def verification(request):
